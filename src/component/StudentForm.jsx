3f277ba1f0eaaef5001abd7a386ae6820cded2e4
vijayrajarathinam <vijayrajarathinam93@gmail.com>
2023-05-27 15:35:23 +0530

    formik chnages

--- src/component/StudentForm.jsx
+++ src/component/StudentForm.jsx
@@ -1,36 +1,79 @@
 import axios from "axios";
+import { Formik } from "formik";
 import React, { useState } from "react";
 
 export default function StudentForm(props) {
-  const [post, setPost] = useState({ title: "", body: "" });
+  // const [post, setPost] = useState();
 
-  const onChangeHandler = (e) => {
-    const { target } = e;
-    const { name, value } = target;
+  // const onChangeHandler = (e) => {
+  //   const { target } = e;
+  //   const { name, value } = target;
 
-    const newPost = { ...post, [name]: value };
+  //   const newPost = { ...post, [name]: value };
 
-    setPost(newPost);
-  };
+  //   setPost(newPost);
+  // };
 
-  function onSubmitHandler(e) {
+  function onSubmitHandler(values, { setSubmitting }) {
     // const { preventDefault } = e;
-    e.preventDefault();
+    // e.preventDefault();
 
     axios
-      .post("https://jsonplaceholder.typicode.com/posts", post)
+      .post("https://jsonplaceholder.typicode.com/posts", values)
       .then((response) => {
         const { data } = response;
+        setSubmitting(false);
         props.setPosts((curr) => [data, ...curr]);
       })
       .catch((err) => console.log(err));
   }
 
+  function validateForm(values) {
+    // [ [title, ""], [body, ""] ]
+    const errors = {};
+    Object.entries(values).forEach((val) => {
+      const [name, value] = val;
+      if (value?.trim() === "") errors[name] = "required";
+    });
+
+    return errors;
+  }
+
   return (
-    <form onSubmit={onSubmitHandler}>
-      <input value={post.title} name="title" onChange={onChangeHandler} />
-      <input value={post.body} name="body" onChange={onChangeHandler} />
-      <button>Submit</button>
-    </form>
+    <Formik
+      initialValues={{ title: "", body: "" }}
+      validate={validateForm}
+      onSubmit={onSubmitHandler}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange: onChangeHandler,
+        handleSubmit,
+        handleBlur,
+        isSubmitting,
+      }) => {
+        return (
+          <form onSubmit={handleSubmit}>
+            <input
+              value={values.title}
+              name="title"
+              onChange={onChangeHandler}
+              onBlur={handleBlur}
+            />
+            {errors.title && touched.title && errors.title}
+            <input
+              value={values.body}
+              name="body"
+              onChange={onChangeHandler}
+              onBlur={handleBlur}
+            />
+            {errors.body && touched.body && errors.body}
+            <button disabled={isSubmitting}>Submit</button>
+          </form>
+        );
+      }}
+    </Formik>
   );
 }
